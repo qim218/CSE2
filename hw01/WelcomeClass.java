593e3add98b0029c70ceb2c43a799de1da5f089a
qim218 <qim218@lehigh.edu>
2015-08-30 22:37:34 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,21 @@
+//////////////////////////
+//define a class
+
+public class WelcomeClass{
+
+//add main method
+
+public static void main(String[ ] args) {
+
+System.out.println("--------------------   ");
+System.out.println("|      WELCOME     |   ");
+System.out.println("--------------------   ");
+System.out.println(" ^  ^  ^  ^  ^  ^  ^   ");
+System.out.println("/ \\/ \\/ \\/ \\/ \\/ \\/ \\  ");
+System.out.println("|-Q--I--M--2--1--8--|  ");
+System.out.println("\\ /\\ /\\ /\\ /\\ /\\ /\\ /  ");
+System.out.println(" v  v  v  v  v  v  v   ");
+System.out.println("This August my wife and I took an eleven-day, 675-mile bike");
+System.out.println("tour on our tandem. We circled Lake Ontario.");
+}
+}
---
+++ lad01-HelloWorld/HelloWorld.java
@@ -0,0 +1,22 @@
+//////////////////////////
+//define a class
+
+public class HelloWorld{
+
+//add main method
+
+public static void main(String[ ] args) {
+
+System.out.println("####################");
+System.out.println("####################");
+System.out.println("#                  #");
+System.out.println("####################");
+System.out.println("#Hello World 2014!!#");
+System.out.println("#                  #");
+System.out.println("####################");
+System.out.println("####################");
+System.out.println("test");
+
+}
+}
+
